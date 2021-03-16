06e1ff598afcb9913a1c0dba520261664da6fa7f
robertsicher <62100753+robertsicher@users.noreply.github.com>
2021-03-16 21:01:40 +0000

    First commit

---
+++ backend/server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dbConfig = require("./app/config/db.config");
+const authUser = require("./app/routes/auth.routes");
+const retriveUser = require("./app/routes/user.routes");
+const app = express();
+var corsOptions = {
+  origin: "http://localhost:3000",
+};
+app.use(cors(corsOptions));
+//Parse requests of content-type - application/json
+app.use(bodyParser.json());
+//Parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+const db = require("./app/models");
+const Role = db.role;
+db.mongoose
+  .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Successfully connect to Monnpm igoDB.");
+  })
+  .catch((err) => {
+    console.error("Connection error", err);
+    process.exit();
+  });
+// Simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to Mern Authentication" });
+});
+//Routes
+app.use("/", authUser);
+app.use("/", retriveUser);
+// Set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
