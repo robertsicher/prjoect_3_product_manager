851d1fa459e328b9b1eab74808899eee30cf22a2
Nikstar-1 <rk_787@hotmail.co.uk>
2021-03-20 17:53:52 +0000

    able to get cloudinary widget to display

--- client/src/components/Cloudinary.js
+++ client/src/components/Cloudinary.js
@@ -17,16 +17,44 @@ const uploadImage = (e) => {
     .then((data) => {});
 };
 
+// class Main extends React.Component {
+//   render() {
+//     return (
+//       <div className="main">
+//         <form method="post" enctype="multipart/form-data">
+//           <input type="file" name="files[]" onChange={uploadImage} multiple />
+//           <input type="submit" value="Upload Files" name="submit" />
+//         </form>
+//       </div>
+//     );
+//   }
+// }
 class Main extends React.Component {
-  render() {
-    return (
-      <div className="main">
-        <form method="post" enctype="multipart/form-data">
-          <input type="file" name="files[]" onChange={uploadImage} multiple />
-          <input type="submit" value="Upload Files" name="submit" />
-        </form>
-      </div>
-    );
+    render() {
+      let widget = window.cloudinary.createUploadWidget(
+        {
+          cloudName: "your_cloud_name_here",
+          uploadPreset: "your_present_here",
+        },
+        (error, result) => {
+          if (!error && result && result.event === "success") {
+            console.log("Done! Here is the image info: ", result.info);
+          }
+        }
+      );
+  
+      return (
+        <div>
+          <button
+            onClick={() => {
+              widget.open();
+            }}
+          >
+            Upload photo
+          </button>
+        </div>
+      );
+    }
   }
-}
+
 export default Main;
